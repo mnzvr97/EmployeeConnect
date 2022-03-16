4cb0d2a11e95f28cbe8f503f609a886d8aa2ee43
mnzvr <mansurjah@gmail.com>
2022-03-16 14:27:38 -0400

    Updating User

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,12 @@
 
 
 $("#add_user").submit(function(event){
-    alert("Data Submitted!")
+    alert("Data Submitted!");
 })
 
+$("#update_user").submit(function(event){
+    event.preventDefault();
+
+    var unindexed_array = $(this).serialzeArray();
+    console.log(unindexed_array)
+})

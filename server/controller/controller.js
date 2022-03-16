b328fe6502489991e7d75ee720cf3f437d2a1ccd
mnzvr <mansurjah@gmail.com>
2022-03-15 22:40:29 -0400

    CRUD Controller Functions

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -33,15 +33,56 @@ exports.create = (req,res)=>{
 
 // return a user
 exports.find = (req,res)=>{
-
+    Userdb.find()
+    .then(user => {
+        res.send(user)
+    })
+    .catch(err => {
+        res.status(500).send({
+            message : err.message || "Some error occured while retrieving information"
+        })
+    })
 }
 
 // update a new user by id
 exports.update = (req,res)=>{
+    if(!req.body){
+        return res
+        .status(400)
+        .send({message : "Updated Data cannot be empry"})
+    }
 
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id, req.body, {useFindandModify: false})
+        .then(data => {
+            if(!data){
+                res.status(404).send({message : 'Cannot Update user with ${id}.'})
+            }else{
+                res.send(data)
+            }
+        })
+         .catch(err =>{
+             res.status(500).send({message : "Eroor updating user information"})
+         })   
 }
 
 //delete user
 exports.delete = (req,res)=>{
+    const id = req.params.id;
 
+    Userdb.findByIdAndDelete(id)
+        .then(data => {
+            if(!data){
+                res.status(404).send({message : 'Cannot delete user with ${id}.'})
+            }else{
+                res.send({
+                    message : "User was deleted successfully!"
+                })
+            }
+        })
+        .catch(err =>{
+            res.status(500).send({
+                message : "Could not delete User with id=" + id
+            });
+        });
 }
